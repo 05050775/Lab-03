81da29fd2cfbb95879c10bf06f35e848771c6b40
05050775 <37134199+05050775@users.noreply.github.com>
2018-03-28 15:45:00 +0800

    Add files via upload

---
+++ Lab-03.2.ino
@@ -0,0 +1,25 @@
+float Vin, Analogin=0, i;
+
+void setup() 
+{
+  Serial.begin(9600);
+ pinMode(14,OUTPUT);
+
+   for (i=0;i<=5;i++)
+  {
+    Analogin = 0.7* Analogin + 0.3* analogRead(14);
+    Vin = Analogin * 2.5 / 4095;
+    Serial.println(Vin);
+    Serial.println(analogRead(14));
+    delay(500);
+
+  }
+
+}
+
+void loop()
+{
+
+  }
+
+
---
+++ Lab-03.ino
@@ -0,0 +1,26 @@
+int val;
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Hello,World!");
+ 
+  pinMode(8,OUTPUT);
+}
+void loop()
+{
+  if(Serial.available())
+  {
+    val = Serial.read();
+    if(val == '0')
+    {
+      digitalWrite(8, LOW);
+      Serial.println("LED OFF");
+    }
+    else if(val == '1')
+    {
+      digitalWrite(8, HIGH);
+      Serial.println("LED ON");
+    }
+  }
+}
+
